aa32ef2de5ff84b2c939989c824f51b3f96dedf0
dominicazuka <visitdominicazuka@gmail.com>
2023-07-18 02:53:07 +0100

    Final Boilerplate

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,52 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const mongoose = require("mongoose");
 require("dotenv/config"); //read env variables  from .env file
 
-const api = process.env.API_URL;
-
 //Middleware configuration
-app.use(express.json())
+app.use(express.json());
+app.use(morgan("tiny"));
 
 // http://localhost:3000/api/v1/
+const api = process.env.API_URL;
+
+const productSchema = mongoose.Schema({
+  name: String,
+  image: String,
+  stock: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+app.post(`${api}/products`, (req, res) => {
+  const product = new Product({
+    name: req.body.name,
+    image: req.body.image,
+    stock: req.body.stock,
+  });
+  product
+    .save()
+    .then((createdProduct) => {
+      res.status(201).json(createdProduct);
+    })
+    .catch((error) => {
+      res.status(500).json({ message: error.message });
+    });
+  console.log(product);
+});
+
+app.get(`${api}/products`, async (req, res) => {
+  const allProducts = await Product.find();
+
+  if (!allProducts) {
+    res.status(500).json({ success: false });
+  }
+  res.send(allProducts);
+});
 
 app.get(`${api}/users`, (req, res) => {
   const user = {
@@ -25,7 +64,21 @@ app.post(`${api}/users`, (req, res) => {
   res.send(newUser);
 });
 
-app.listen(4000, () => {
+//Connect the database
+mongoose
+  .connect(process.env.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database connected");
+  })
+  .catch((error) => {
+    console.log("Error" + error);
+  });
+
+//Start the server
+app.listen(3000, () => {
   console.log(`Hello API Test is working from .env file + ${api}`);
   console.log("listening on port http://localhost:3000");
 });
