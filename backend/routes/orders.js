304626599093caab56c84a68d9e07663173a6319
dominicazuka <visitdominicazuka@gmail.com>
2023-07-25 17:26:52 +0100

    Final Project

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -2,20 +2,28 @@ const { Order } = require("../models/order");
 const { OrderItem } = require("../models/order-item");
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
 
 //Get all orders in DB
 router.get("/", async (req, res) => {
-  const orderList = await Order.find().populate('user', 'name').sort({'dateOrdered': -1});
+  const orderList = await Order.find()
+    .populate("user", "name")
+    .sort({ dateOrdered: -1 });
 
   if (!orderList) {
     res.status(500).json({ success: false });
   }
-  res.send(orderList); 
+  res.send(orderList);
 });
 
 //Get order in DB by ID
 router.get("/:id", async (req, res) => {
-  const order = await Order.findById(req.params.id).populate('user', 'name').populate({path: 'orderItems', populate:{path: "product", populate: 'category'}})
+  const order = await Order.findById(req.params.id)
+    .populate("user", "name")
+    .populate({
+      path: "orderItems",
+      populate: { path: "product", populate: "category" },
+    });
   if (!order) {
     res.status(500).json({ success: false });
   }
@@ -60,6 +68,60 @@ router.post("/", async (req, res) => {
     res
       .status(500)
       .json({ message: "Sorry an error occurred, please try again." });
+  }
+});
+
+// Update status of order
+router.put("/:id", async (req, res) => {
+  try {
+    if (!mongoose.isValidObjectId(req.params.id)) {
+      return res.status(404).send("Invalid Order ID");
+    }
+    const order = await Order.findByIdAndUpdate(
+      req.params.id,
+      {
+        status: req.body.status,
+      },
+      { new: true }
+    );
+    if (!order) return res.status(404).send("The order cannot be updated");
+    res.send(order);
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({ message: "Sorry an error occurred, please try again." });
+  }
+});
+
+// Delete order
+router.delete("/:id", async (req, res) => {
+  try {
+    if (!mongoose.isValidObjectId(req.params.id)) {
+      return res.status(404).send("Invalid Order ID");
+    }
+    const order = await Order.findByIdAndDelete(req.params.id);
+    if (order) {
+      const deleteOrderItems = await order.orderItems.map(async (orderItem) => {
+        await OrderItem.findByIdAndDelete(orderItem);
+      });
+      res
+        .status(200)
+        .json({ success: true, message: "Order deleted successfully" });
+    } else {
+      res.status(404).json({
+        success: false,
+        message: "Order not found",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res
+      .status(500)
+      .json({
+        success: false,
+        message: "Sorry an error occurred, please try again.",
+      });
   }
 });
 
